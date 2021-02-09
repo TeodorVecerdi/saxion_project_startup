c1942c7d2bf2ad906eb9756cc465c40d5b5fb4c3
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-09 19:04:02 +0200

    Implement profile route

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -3,7 +3,23 @@ let router = express.Router();
 const serverState = require("../js/ServerState");
 
 router.get('/', (req, res) => {
+    if(!serverState.isAuthenticated(req)) {
+        res.redirect("/login");
+        return;
+    }
+    let cookie = req.cookies['logged-in'];
+    if(!serverState.isCookieValid(cookie) || !serverState.userExists(cookie)) {
+        res.clearCookie('logged-in');
+        res.redirect("/login");
+        return;
+    }
+
     res.render("pages/profile")
 })
+
+router.post('/', (req, res) => {
+    serverState.setupProfile(req);
+    res.status(200).end();
+});
 
 module.exports = {url: "/profile", router: router};
