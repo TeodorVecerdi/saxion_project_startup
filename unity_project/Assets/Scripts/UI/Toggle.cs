632571b2c7998e2a888d0d6a98f97901c47d3967
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-11 19:10:21 +0200

    Add Toggle controller

---
+++ unity_project/Assets/Scripts/UI/Toggle.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Toggle : MonoBehaviour {
+    [SerializeField] private float AnimationDuration = 0.25f;
+    [SerializeField] private Color ActiveColor;
+    [SerializeField] private Color InactiveColor;
+    [SerializeField] private Image TargetGraphic;
+    [SerializeField] private bool Active;
+    [SerializeField] private bool CanBeDisabled;
+    [SerializeField] private List<Toggle> ToggleGroup;
+    [SerializeField] private UnityEvent OnSetActive;
+
+    private bool isActive;
+    
+    private void Start() {
+        TargetGraphic.color = Active ? ActiveColor : InactiveColor;
+        isActive = Active;
+    }
+
+    private void SetState(bool state) {
+        if(isActive == state) return;
+        
+        foreach (var toggle in ToggleGroup) {
+            if(toggle != this)
+                toggle.SetState(false);
+        }
+        isActive = state;
+        TweenSequence();
+        if(isActive && OnSetActive != null) OnSetActive.Invoke();
+    }
+
+    public void ToggleState() {
+        if(!CanBeDisabled && isActive) return;
+        SetState(!isActive);
+    }
+
+    private void TweenSequence() {
+        TargetGraphic.DOColor(isActive ? ActiveColor : InactiveColor, AnimationDuration);
+    }
+}
+
