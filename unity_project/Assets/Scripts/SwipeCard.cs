b0692dc8ac25089d5e2532f419ea87901eec6274
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-11 15:19:51 +0200

    Add picture slide control and swipe confirmation

--- unity_project/Assets/Scripts/SwipeCard.cs
+++ unity_project/Assets/Scripts/SwipeCard.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
+using UnityCommons;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class SwipeCard : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
     [SerializeField] private float MaxDrag = 9f;
@@ -9,7 +12,14 @@ public class SwipeCard : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDra
     [SerializeField] private float PositionMultiplier = 2.5f;
     [Space, SerializeField] private GameObject Nope;
     [SerializeField] private GameObject Yes;
-    
+    [Space]
+    [SerializeField] private List<ImageIndicatorPair> Slides;
+    [SerializeField] private Color ActiveColor;
+    [SerializeField] private Color InactiveColor;
+    private bool confirmed = false;
+    private bool confirmValue = false;
+
+    private int currentSlide;
     private RectTransform rectTransform;
     private float startDrag;
 
@@ -19,6 +29,7 @@ public class SwipeCard : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDra
         rectTransform = GetComponent<RectTransform>();
 
         baseAnchoredY = rectTransform.anchoredPosition.y;
+        SetActiveSlide(0);
     }
 
     public void OnDrag(PointerEventData eventData) {
@@ -32,8 +43,13 @@ public class SwipeCard : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDra
         rectTransform.eulerAngles = new Vector3(0, 0, -dragAmount);
         
         // Check threshold
-        if (Nope != null) Nope.SetActive(dragAmount <= -MaxDrag * ConfirmThreshold);
-        if (Yes != null) Yes.SetActive(dragAmount >= MaxDrag * ConfirmThreshold);
+        var confirmNope = dragAmount <= -MaxDrag * ConfirmThreshold;
+        var confirmYes = dragAmount >= MaxDrag * ConfirmThreshold;
+        if (Nope != null) Nope.SetActive(confirmNope);
+        if (Yes != null) Yes.SetActive(confirmYes);
+
+        confirmed = confirmNope || confirmYes;
+        confirmValue = confirmYes;
     }
 
     public void OnBeginDrag(PointerEventData eventData) {
@@ -46,6 +62,29 @@ public class SwipeCard : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDra
         
         if (Nope != null) Nope.SetActive(false);
         if (Yes != null) Yes.SetActive(false);
+
+        if (confirmed) {
+            Debug.Log($"You chose: {(confirmValue ? "YES" : "NO")}");
+        }
+    }
+
+    private void SetActiveSlide(int index) {
+        Slides[currentSlide].Slide.SetActive(false);
+        Slides[currentSlide].Indicator.color = InactiveColor;
+        currentSlide = index;
+        Slides[currentSlide].Slide.SetActive(true);
+        Slides[currentSlide].Indicator.color = ActiveColor;
+    }
+
+    public void ChangeSlide(int direction) {
+        int newSlide = (currentSlide + direction).Clamped(0, Slides.Count-1);
+        SetActiveSlide(newSlide);        
+    }
+
+    [Serializable]
+    class ImageIndicatorPair {
+        public GameObject Slide;
+        public Image Indicator;
     }
 }
 
