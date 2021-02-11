952d3cffdd14de5788122e0166f16e7164ad4066
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-11 19:10:12 +0200

    Add slide controller

---
+++ unity_project/Assets/Scripts/UI/Slide/Slide.cs
@@ -0,0 +1,31 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+
+public class Slide : MonoBehaviour {
+    [SerializeField] private float ExitPosition = 1170f;
+    [SerializeField] private float AnimationDuration = 0.5f;
+    [SerializeField] private GameObject RaycastBlocker;
+
+    private RectTransform rectTransform;
+
+    private void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    public void SetActive(bool isActive) {
+        RaycastBlocker.SetActive(!isActive);
+        
+        if(isActive) EnableTweenSequence();
+        else DisableTweenSequence();
+    }
+
+    private void EnableTweenSequence() {
+        rectTransform.DOAnchorPosX(0f, AnimationDuration, true).From(new Vector2(ExitPosition, 0));
+    }
+
+    private void DisableTweenSequence() {
+        rectTransform.DOAnchorPosX(-ExitPosition, AnimationDuration, true).From(new Vector2(0, 0));
+    }
+}
+
