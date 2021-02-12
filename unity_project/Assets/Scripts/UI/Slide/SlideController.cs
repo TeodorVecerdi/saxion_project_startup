18b9f412b7e87af84dd3df67faf62e0a90a1f13f
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-12 14:21:52 +0200

    Add slide controller for game list

---
+++ unity_project/Assets/Scripts/UI/Slide/SlideController.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideController : MonoBehaviour {
+    [SerializeField] private List<Slide> Slides;
+    [SerializeField] private bool WrapAtEnd;
+    private int currentSlide;
+
+    public void NextSlide() {
+        var newSlide = currentSlide + 1;
+        if (newSlide >= Slides.Count) {
+            if (WrapAtEnd) newSlide = 0;
+            else return;
+        }
+
+        Slides[currentSlide].SetActive(false);
+        currentSlide = newSlide;
+        Slides[currentSlide].SetActive(true);
+    }
+
+    public void PreviousSlide() {
+        var newSlide = currentSlide - 1;
+
+        if (newSlide < 0) {
+            if (WrapAtEnd) newSlide = Slides.Count - 1;
+            else return;
+        }
+
+        Slides[currentSlide].SetActive(false, true);
+        currentSlide = newSlide;
+        Slides[currentSlide].SetActive(true, true);
+    }
+}
