57cc2f7eb77724bd8cc90c8b3daa93129e4cc4c7
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-12 14:22:28 +0200

    Add scripting implementation for stage 2 of onboarding

--- unity_project/Assets/Scripts/UI/OnboardingController.cs
+++ unity_project/Assets/Scripts/UI/OnboardingController.cs
@@ -1,37 +1,39 @@
 ﻿using System;
 using DG.Tweening;
+using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class OnboardingController : MonoBehaviour {
-    [SerializeField] private Image GradientImage;
-    [SerializeField] private Image BackgroundImage;
-    [SerializeField] private float ColorChangeDuration = 0.5f;
-    [Space]
-    [SerializeField] private Color PinkGradient;
-    [SerializeField] private Color PinkBackground;
-    [Space]
-    [SerializeField] private Color GreenGradient;
-    [SerializeField] private Color GreenBackground;
-    [Space]
-    [SerializeField] private Color BlueGradient;
-    [SerializeField] private Color BlueBackground;
+    [Foldout("Color Scheme"), SerializeField] private Image GradientImage;
+    [Foldout("Color Scheme"), SerializeField] private Image BackgroundImage;
+    [Foldout("Color Scheme"), SerializeField] private float ColorChangeDuration = 0.5f;
+    [Foldout("Color Scheme"), Space, SerializeField] private Color PinkGradient;
+    [Foldout("Color Scheme"), SerializeField] private Color PinkBackground;
+    [Foldout("Color Scheme"), Space, SerializeField] private Color GreenGradient;
+    [Foldout("Color Scheme"), SerializeField] private Color GreenBackground;
+    [Foldout("Color Scheme"), Space, SerializeField] private Color BlueGradient;
+    [Foldout("Color Scheme"), SerializeField] private Color BlueBackground;
     [Space]
     [SerializeField] private Button RelationshipPreferenceButton;
     [SerializeField] private Button GenderPreferenceButton;
     [SerializeField] private Button AboutNextButton;
-    
+    [SerializeField] private Button GameGenreButton;
+    [SerializeField] private Button GamesPlayedButton;
+
     private Gender gender = Gender.Male;
     private GenderPreference genderPreference = GenderPreference.None;
     private RelationshipPreference relationshipPreference = RelationshipPreference.None;
     private string name;
     private Date birthDate;
     private string about;
+    private GameGenre gameGenres = GameGenre.None;
+    private GamePlayed gamesPlayed = GamePlayed.None;
 
     public void OnGenderChange(int mask) {
         gender = (Gender) Enum.ToObject(typeof(Gender), mask);
     }
-    
+
     public void OnRelationshipPreferenceChange(int mask) {
         relationshipPreference = (RelationshipPreference) Enum.ToObject(typeof(RelationshipPreference), mask);
         RelationshipPreferenceButton.interactable = relationshipPreference != RelationshipPreference.None;
@@ -40,6 +42,16 @@ public class OnboardingController : MonoBehaviour {
     public void OnGenderPreferenceChange(int mask) {
         genderPreference = (GenderPreference) Enum.ToObject(typeof(GenderPreference), mask);
         GenderPreferenceButton.interactable = genderPreference != GenderPreference.None;
+    }
+
+    public void OnGameGenreChange(int mask) {
+        gameGenres = (GameGenre) Enum.ToObject(typeof(GameGenre), mask);
+        GameGenreButton.interactable = gameGenres != GameGenre.None;
+    }
+    
+    public void OnGamesPlayedChange(int mask) {
+        gamesPlayed = (GamePlayed) Enum.ToObject(typeof(GamePlayed), mask);
+        GamesPlayedButton.interactable = gamesPlayed != GamePlayed.None;
     }
 
     public void OnNameChange(string newName) {
@@ -51,12 +63,12 @@ public class OnboardingController : MonoBehaviour {
         birthDate.Day = newDay;
         CheckAboutValid();
     }
-    
+
     public void OnMonthChange(string newMonth) {
         birthDate.Month = newMonth;
         CheckAboutValid();
     }
-    
+
     public void OnYearChange(string newYear) {
         birthDate.Year = newYear;
         CheckAboutValid();
@@ -65,7 +77,7 @@ public class OnboardingController : MonoBehaviour {
     public void OnAboutChange(string newAbout) {
         about = newAbout;
     }
-    
+
     private void CheckAboutValid() {
         var valid = !string.IsNullOrWhiteSpace(name) &&
                     !string.IsNullOrWhiteSpace(birthDate.Day) &&
