99e04146e470333688f6efd4973defced7d5b566
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-12 14:22:10 +0200

    Add game genre and game played enums

---
+++ unity_project/Assets/Scripts/Models/User Model/GamePlayed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+[Flags]
+public enum GamePlayed {
+    None = 0,
+    League = 1 << 0,
+    Rust = 1 << 1,
+    GTA = 1 << 2,
+    Fortnite = 1 << 3,
+    COD = 1 << 4,
+    Apex = 1 << 5,
+    CSGO = 1 << 6,
+    Valorant = 1 << 7
+}
