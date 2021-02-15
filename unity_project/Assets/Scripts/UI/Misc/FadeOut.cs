0c24693e43aaf75bab413f55dcb76f31142b1a27
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-15 12:56:52 +0200

    Add OnBeforeFade(In/Out) events to fader

--- unity_project/Assets/Scripts/UI/Misc/FadeOut.cs
+++ unity_project/Assets/Scripts/UI/Misc/FadeOut.cs
@@ -8,10 +8,13 @@ public class FadeOut : MonoBehaviour {
     [SerializeField] private List<Graphic> Graphics;
     [SerializeField] private float Duration;
     [SerializeField] private UnityEvent OnFadeIn;
+    [SerializeField] private UnityEvent OnBeforeFadeIn;
     [SerializeField] private UnityEvent OnFadeOut;
+    [SerializeField] private UnityEvent OnBeforeFadeOut;
 
     public void Fade(bool state) {
         if(Graphics.Count <= 0) return;
+        (state ? OnBeforeFadeOut : OnBeforeFadeIn)?.Invoke();
         Graphics[0].DOFade(state ? 0 : 1, Duration).From(state ? 1 : 0).OnComplete(() => { (state ? OnFadeOut : OnFadeIn)?.Invoke(); });
         for (var index = 1; index < Graphics.Count; index++) {
             var graphic = Graphics[index];
