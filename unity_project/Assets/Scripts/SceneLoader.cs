0c216cc82a72b31b6f723ceb8a533cf0b259eefe
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-15 13:37:17 +0200

    Add Scene loader

---
+++ unity_project/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using UnityCommons;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoSingleton<SceneLoader> {
+    protected override void OnAwake() {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadScene(int sceneIndex, Action callback) {
+        StartCoroutine(LoadSceneImpl(sceneIndex, callback));
+    }
+
+    private IEnumerator LoadSceneImpl(int sceneIndex, Action callback) {
+        
+        var operation = SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Additive);
+        
+        while (!operation.isDone) {
+            yield return null;
+        }
+        
+        callback?.Invoke();
+    }
+}
---
+++ unity_project/Assets/Scripts/Scenes.cs
@@ -0,0 +1,5 @@
+﻿public static class Scenes {
+    public const int LoginRegister = 0;
+    public const int Onboarding = 1;
+    public const int SwipeMenu = 2;
+}
