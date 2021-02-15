cfa5e7fdcdb93a1435f54c3b899f635c554fc300
Vlad Ilie Miu <miu.vlad00@gmail.com>
2021-02-15 12:58:27 +0200

    chatBubble almost

---
+++ unity_project/Assets/Scripts/UI/Misc/ChatBubble.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using TMPro;
+using Unity;
+using Unity.VectorGraphics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatBubble : MonoBehaviour
+{
+    [SerializeField] private SVGImage chatBubble;
+
+    [SerializeField] private TMP_Text text;
+
+    private void Start()
+    {
+        var fontSize = text.fontSize;
+    }
+
+    private void OnInputText()
+    {
+        
+    }
+}
