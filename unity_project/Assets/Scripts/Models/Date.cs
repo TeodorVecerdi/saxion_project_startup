5c2e1864c2e38f5a8e5d98ff190b0d940dbbc73a
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-16 16:17:12 +0200

    Add date to age converter

--- unity_project/Assets/Scripts/Models/Date.cs
+++ unity_project/Assets/Scripts/Models/Date.cs
@@ -5,4 +5,15 @@ public struct Date {
     public string Day;
     public string Month;
     public string Year;
+
+    public static int Age(Date date) {
+        var currentDate = DateTime.Now;
+        var (day, month, year) = (int.Parse(date.Day), int.Parse(date.Month), int.Parse(date.Year));
+
+        var baseAge = currentDate.Year - year;
+        if (month > currentDate.Month) baseAge--;
+        else if (month == currentDate.Month && day > currentDate.Day) baseAge--;
+
+        return baseAge;
+    }
 }
