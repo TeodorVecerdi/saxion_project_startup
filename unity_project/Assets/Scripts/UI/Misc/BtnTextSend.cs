d4a414cf34791b96acb9cb8f7659716fe3ff0fdd
Vlad Ilie Miu <miu.vlad00@gmail.com>
2021-02-16 17:19:10 +0200

    Added chatBubble script for resizing and chatbubble prefab

---
+++ unity_project/Assets/Scripts/UI/Misc/BtnTextSend.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using Unity;
+using TMPro;
+
+[RequireComponent(typeof(TMP_InputField))]
+public class BtnTextSend : MonoBehaviour
+{
+    private TMP_InputField inputField;
+    [SerializeField]
+    private TMP_InputField target;
+
+    private void Awake()
+    {
+        inputField = gameObject.GetComponent<TMP_InputField>();
+    }
+
+    public void OnClick()
+    {
+        target.text = inputField.text;
+    }
+}
---
+++ unity_project/Assets/Scripts/UI/Misc/ChatBubbles.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Unity;
+using TMPro;
+
+[RequireComponent(typeof(TMP_Text))]
+public class ChatBubbles : MonoBehaviour
+{
+    private TMP_Text text;
+    
+    
+}
