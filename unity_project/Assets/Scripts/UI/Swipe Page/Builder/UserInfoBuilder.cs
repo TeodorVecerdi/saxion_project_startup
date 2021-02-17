8ecc9aff2732821abbc4c2c64664444c3abf3ffe
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-17 17:32:59 +0200

    Add null check to UserInfoBuilder

--- unity_project/Assets/Scripts/UI/Swipe Page/Builder/UserInfoBuilder.cs
+++ unity_project/Assets/Scripts/UI/Swipe Page/Builder/UserInfoBuilder.cs
@@ -7,10 +7,13 @@ public class UserInfoBuilder : MonoBehaviour, IBuilder {
     [SerializeField] private TextMeshProUGUI About;
 
     public void Build(UserModel model) {
-        Name.text = model.Name;
-        var age = Date.Age(model.BirthDate);
-        Age.text = $"Age: {age}";
-        About.text = model.About;
+        if (Name != null) Name.text = model.Name;
+        if (Age != null) {
+            var age = Date.Age(model.BirthDate);
+            Age.text = $"Age: {age}";
+        }
+
+        if (About != null) About.text = model.About;
     }
 
     public void Cleanup() {
