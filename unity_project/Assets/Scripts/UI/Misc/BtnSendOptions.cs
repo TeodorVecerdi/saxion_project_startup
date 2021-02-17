3b8822d9290711bb3275fbd70bafc14a719d8b7d
Vlad Ilie Miu <miu.vlad00@gmail.com>
2021-02-17 19:48:24 +0200

    Added functionality on the side of the user filling in the miniGame

---
+++ unity_project/Assets/Scripts/UI/Misc/BtnSendOptions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity;
+using TMPro;
+using UnityEngine.Rendering;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+[RequireComponent(typeof(Button))]
+public class BtnSendOptions : MonoBehaviour
+{
+    [SerializeField] private GameObject inputPanel;
+    [SerializeField]
+    private TMP_InputField truth1;
+    [SerializeField] 
+    private TMP_InputField truth2;
+    [SerializeField]
+    private TMP_InputField lie;
+    [SerializeField] [Tooltip("Please use 3 buttons")]
+    private List<TMP_Text> outputButtons; 
+    
+    public void OnClick()
+    {
+        var buttonCount = outputButtons.Count;
+        var randomIndex = Random.Range(0, buttonCount - 1);
+        outputButtons[randomIndex].text = lie.text;
+        outputButtons.Remove(outputButtons[randomIndex]);
+        randomIndex = Random.Range(0, buttonCount - 1);
+        outputButtons[randomIndex].text = truth1.text;
+        outputButtons.Remove(outputButtons[randomIndex]);
+        randomIndex = 0;
+        outputButtons[randomIndex].text = truth2.text;
+        
+        inputPanel.SetActive(false);
+    }
+}
