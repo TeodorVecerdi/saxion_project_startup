b0990ec3b7e679b2b7b21e213fb16a80f2eb1852
Vlad Ilie Miu <miu.vlad00@gmail.com>
2021-02-19 16:47:50 +0200

    Customizable popup, hardcoded center screen

--- unity_project/Assets/Scripts/UI/Misc/SpawnPopup.cs
+++ unity_project/Assets/Scripts/UI/Misc/SpawnPopup.cs
@@ -1,23 +1,23 @@
-﻿using TMPro;
+﻿using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class SpawnPopup : MonoBehaviour
 {
     [SerializeField]private GameObject popupPrefab;
-    [SerializeField]private string popupText;
-    private GameObject _clickedBtn;
+    [SerializeField]private string titleText = "Sorry";
+    [SerializeField]private string thingNotFree = "This feature";
+    [SerializeField]private string followUpText = "is unavailable currently. Buy ScoreMe gold to unlock this feature.";
 
-    // private void Initialize() {
-    //     
-    // }
-
-    public void OnClick()
+    public void ShowPopup()
     {
-        _clickedBtn = this.gameObject;
-        var popupObj = Instantiate(popupPrefab, transform.parent);
-        var textComp = popupObj.GetComponent<TMP_Text>();
-        textComp.text = _clickedBtn + popupText;
-        
+        var popupObj = Instantiate(popupPrefab, new Vector3(325, 644), Quaternion.identity, transform.parent); //hardcode yay
+        var titleComponent = popupObj.transform.GetChild(0).GetComponent<TMP_Text>();
+        var txtComponent = popupObj.transform.GetChild(1).GetComponent<TMP_Text>();
+
+        titleComponent.text = titleText;
+        txtComponent.text = thingNotFree + " " + followUpText;
     }
     
 }
