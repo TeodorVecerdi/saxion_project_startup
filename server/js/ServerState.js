176d0de1c22c3f04e45c6d817dbccc1dd4b2d9b1
Teodor Vecerdi <teodor.vecerdi@gmail.com>
2021-02-19 16:59:27 +0200

    Implement minigame on server

--- server/js/ServerState.js
+++ server/js/ServerState.js
@@ -26,7 +26,7 @@ function makeProfile(jsonString) {
         profilePictureType: values["profilePictureType"]
     }
 
-    if(profile.profilePictureType === 0) {
+    if (profile.profilePictureType === 0) {
         profile["avatar"] = values["avatar"];
     } else {
         profile["pictures"] = values["profilePictures"]
@@ -63,37 +63,32 @@ class ServerState {
 
     createBackup() {
         let backupJSON = this.getJSON();
-
-        fs.writeFile("./data/backup.json", backupJSON, err => {
-            if(err) throw err;
-            console.log("Backup success!");
-        });
+        fs.writeFileSync("./data/backup.json", backupJSON);
     }
 
     restoreBackup() {
         fs.access("./data/backup.json", err => {
             if (err === null) {
-                fs.readFile("./data/backup.json", (err1, data) => {
-                    if(err1) throw err1;
-                    let backupJSON = JSON.parse(data);
+                let data = fs.readFileSync("./data/backup.json");
+                let backupJSON = JSON.parse(data);
 
-                    this.registeredUsers = backupJSON.registeredUsers || [];
-                    this.messages = backupJSON.messages || {};
-                    this.unconfirmedMessages = backupJSON.unconfirmedMessages || {};
-                    this.swipes = backupJSON.swipes || {};
-                    this.matches = backupJSON.matches || {};
-                    this.unconfirmedMatches = backupJSON.unconfirmedMatches || {};
-                    this.gameRequests = backupJSON.gameRequests || {};
-                    this.games = backupJSON.games || {};
-                    console.log("Backup restored successfully!")
-                });
+                this.registeredUsers = backupJSON.registeredUsers || [];
+                this.messages = backupJSON.messages || {};
+                this.unconfirmedMessages = backupJSON.unconfirmedMessages || {};
+                this.swipes = backupJSON.swipes || {};
+                this.matches = backupJSON.matches || {};
+                this.unconfirmedMatches = backupJSON.unconfirmedMatches || {};
+                this.gameRequests = backupJSON.gameRequests || {};
+                this.games = backupJSON.games || {};
+                console.log("Backup restored successfully!")
+
             }
         })
     }
 
     clearBackup() {
         fs.writeFile("./data/backup.json", "{}", err => {
-            if(err) throw err;
+            if (err) throw err;
             console.log("Cleared backup successfully!");
             this.restoreBackup()
         });
@@ -109,7 +104,7 @@ class ServerState {
 
     getUsers() {
         let users = [];
-        for(let key of Object.keys(this.registeredUsers)) {
+        for (let key of Object.keys(this.registeredUsers)) {
             let user = v8.deserialize(v8.serialize(this.registeredUsers[key]));
             delete user['password'];
             users.push(user);
@@ -118,25 +113,30 @@ class ServerState {
     }
 
     usernameExists(username) {
-        for(let account of this.registeredUsers) {
-            if(account.username === username) return true;
+        for (let account of this.registeredUsers) {
+            if (account.username === username) return true;
         }
         return false;
     }
+
     registerUser(username, password) {
         let account = makeAccount(username, password);
         this.registeredUsers.push(account);
         this.createBackup();
     }
+
     passwordMatches(username, password) {
         return this.registeredUsers.find(account => account.username === username).password === password;
     }
+
     getUserByUsername(username) {
         return this.registeredUsers.find(account => account.username === username)
     }
+
     getUserByID(ID) {
         return this.registeredUsers.find(account => account.id === ID)
     }
+
     setupProfile(request) {
         let userId = request.body.id;
         let profile = makeProfile(request.body.profile);
@@ -147,22 +147,23 @@ class ServerState {
     }
 
     swipe(from, to, swipe) {
-        if(!this.swipes.hasOwnProperty(from))
+        if (!this.swipes.hasOwnProperty(from))
             this.swipes[from] = {};
         this.swipes[from][to] = swipe;
         this.createBackup();
 
         this.checkMatches(from, to);
     }
+
     checkMatches(from, to) {
-        if(!this.swipes.hasOwnProperty(from) || !this.swipes.hasOwnProperty(to) || !this.swipes[from].hasOwnProperty(to) || !this.swipes[to].hasOwnProperty(from)) return;
+        if (!this.swipes.hasOwnProperty(from) || !this.swipes.hasOwnProperty(to) || !this.swipes[from].hasOwnProperty(to) || !this.swipes[to].hasOwnProperty(from)) return;
 
-        if(this.swipes[from][to] == 1 && this.swipes[to][from] == 1) {
-            if(!this.matches.hasOwnProperty(from)) this.matches[from] = {};
-            if(!this.matches.hasOwnProperty(to)) this.matches[to] = {};
+        if (this.swipes[from][to] == 1 && this.swipes[to][from] == 1) {
+            if (!this.matches.hasOwnProperty(from)) this.matches[from] = {};
+            if (!this.matches.hasOwnProperty(to)) this.matches[to] = {};
 
-            if(!this.unconfirmedMatches.hasOwnProperty(from)) this.unconfirmedMatches[from] = {};
-            if(!this.unconfirmedMatches.hasOwnProperty(to)) this.unconfirmedMatches[to] = {};
+            if (!this.unconfirmedMatches.hasOwnProperty(from)) this.unconfirmedMatches[from] = {};
+            if (!this.unconfirmedMatches.hasOwnProperty(to)) this.unconfirmedMatches[to] = {};
 
 
             this.matches[from][to] = {};
@@ -173,39 +174,45 @@ class ServerState {
             this.createBackup();
         }
     }
+
     hasMatch(from, to) {
-        if(!this.matches.hasOwnProperty(from) || !this.matches.hasOwnProperty(to) || !this.matches[from].hasOwnProperty(to) || !this.matches[to].hasOwnProperty(from)) return false;
+        if (!this.matches.hasOwnProperty(from) || !this.matches.hasOwnProperty(to) || !this.matches[from].hasOwnProperty(to) || !this.matches[to].hasOwnProperty(from)) return false;
         return true;
     }
+
     getMatches(userId) {
-        if(!this.matches.hasOwnProperty(userId)) return [];
+        if (!this.matches.hasOwnProperty(userId)) return [];
         return Object.keys(this.matches[userId]);
     }
+
     getSwipes(userId) {
-        if(!this.swipes.hasOwnProperty(userId)) return [];
+        if (!this.swipes.hasOwnProperty(userId)) return [];
         let swipes = [];
-        for(let key of Object.keys(this.swipes[userId])) {
+        for (let key of Object.keys(this.swipes[userId])) {
             swipes.push({"id": key, "swipe": this.swipes[userId][key]})
         }
         return swipes;
     }
+
     getUnconfirmedMatches(userId) {
-        if(!this.unconfirmedMatches.hasOwnProperty(userId)) return [];
+        if (!this.unconfirmedMatches.hasOwnProperty(userId)) return [];
         return Object.keys(this.unconfirmedMatches[userId]);
     }
-    confirmMatches(userId, ids) {
-        if(!this.unconfirmedMatches.hasOwnProperty(userId)) return;
 
-        for(let id of ids) {
-            if(this.unconfirmedMatches[userId].hasOwnProperty(id))
+    confirmMatches(userId, ids) {
+        if (!this.unconfirmedMatches.hasOwnProperty(userId)) return;
+
+        for (let id of ids) {
+            if (this.unconfirmedMatches[userId].hasOwnProperty(id))
                 delete this.unconfirmedMatches[userId][id];
         }
         this.createBackup();
     }
+
     getLikesCount(userId) {
         let likesCount = 0;
-        for(let id of Object.keys(this.swipes)) {
-            if(this.swipes[id].hasOwnProperty(userId) && this.swipes[id][userId] == 1)
+        for (let id of Object.keys(this.swipes)) {
+            if (this.swipes[id].hasOwnProperty(userId) && this.swipes[id][userId] == 1)
                 likesCount++;
         }
         return likesCount;
@@ -216,10 +223,10 @@ class ServerState {
         let to = req.body.to;
         let message = req.body.message;
 
-        if(!this.messages.hasOwnProperty(from)) this.messages[from] = {};
-        if(!this.messages[from].hasOwnProperty(to)) this.messages[from][to] = {};
-        if(!this.messages.hasOwnProperty(to)) this.messages[to] = {};
-        if(!this.messages[to].hasOwnProperty(from)) this.messages[to][from] = {};
+        if (!this.messages.hasOwnProperty(from)) this.messages[from] = {};
+        if (!this.messages[from].hasOwnProperty(to)) this.messages[from][to] = {};
+        if (!this.messages.hasOwnProperty(to)) this.messages[to] = {};
+        if (!this.messages[to].hasOwnProperty(from)) this.messages[to][from] = {};
 
         this.messages[from][to][msgId] = {
             id: msgId,
@@ -230,26 +237,28 @@ class ServerState {
         }
         this.messages[to][from][msgId] = this.messages[from][to][msgId];
 
-        if(!this.unconfirmedMessages.hasOwnProperty(to)) this.unconfirmedMessages[to] = {};
-        if(!this.unconfirmedMessages[to].hasOwnProperty(from)) this.unconfirmedMessages[to][from] = {};
+        if (!this.unconfirmedMessages.hasOwnProperty(to)) this.unconfirmedMessages[to] = {};
+        if (!this.unconfirmedMessages[to].hasOwnProperty(from)) this.unconfirmedMessages[to][from] = {};
         this.unconfirmedMessages[to][from][msgId] = {};
 
         this.createBackup();
     }
+
     confirmMessages(from, to, ids) {
-        if(!this.unconfirmedMessages.hasOwnProperty(from)) return;
-        if(!this.unconfirmedMessages[from].hasOwnProperty(to)) return;
+        if (!this.unconfirmedMessages.hasOwnProperty(from)) return;
+        if (!this.unconfirmedMessages[from].hasOwnProperty(to)) return;
 
         for (let id of ids) {
-            if(this.unconfirmedMessages[from][to].hasOwnProperty(id))
+            if (this.unconfirmedMessages[from][to].hasOwnProperty(id))
                 delete this.unconfirmedMessages[from][to][id];
         }
 
         this.createBackup();
     }
+
     confirmAllMessages(from, to) {
-        if(!this.unconfirmedMessages.hasOwnProperty(from)) return;
-        if(!this.unconfirmedMessages[from].hasOwnProperty(to)) return;
+        if (!this.unconfirmedMessages.hasOwnProperty(from)) return;
+        if (!this.unconfirmedMessages[from].hasOwnProperty(to)) return;
 
         for (let id of Object.keys(this.unconfirmedMessages[from][to])) {
             delete this.unconfirmedMessages[from][to][id];
@@ -257,26 +266,28 @@ class ServerState {
 
         this.createBackup();
     }
+
     getMessages(from, to) {
         if (!this.messages.hasOwnProperty(from) || !this.messages[from].hasOwnProperty(to)) return [];
         let messages = [];
-        for(let id of Object.keys(this.messages[from][to])) {
+        for (let id of Object.keys(this.messages[from][to])) {
             messages.push(this.messages[from][to][id])
         }
         return messages;
     }
+
     getUnconfirmedMessages(from, to) {
         if (!this.unconfirmedMessages.hasOwnProperty(from) || !this.unconfirmedMessages[from].hasOwnProperty(to)) return [];
         let messages = [];
-        for(let id of Object.keys(this.unconfirmedMessages[from][to])) {
+        for (let id of Object.keys(this.unconfirmedMessages[from][to])) {
             messages.push(this.messages[from][to][id]);
         }
         return messages;
     }
 
     requestGame(from, to) {
-        if(!this.gameRequests.hasOwnProperty(from)) this.gameRequests[from] = {};
-        if(!this.gameRequests.hasOwnProperty(to)) this.gameRequests[to] = {};
+        if (!this.gameRequests.hasOwnProperty(from)) this.gameRequests[from] = {};
+        if (!this.gameRequests.hasOwnProperty(to)) this.gameRequests[to] = {};
         let gameRequest = {
             initiator: from,
             other: to,
@@ -286,50 +297,125 @@ class ServerState {
         this.gameRequests[to][from] = gameRequest;
         this.createBackup()
     }
+
     getGameRequest(from, to) {
-        if(!this.gameRequests.hasOwnProperty(from) || !this.gameRequests[from].hasOwnProperty(to)) return "null";
+        if (!this.gameRequests.hasOwnProperty(from) || !this.gameRequests[from].hasOwnProperty(to)) return "null";
         return this.gameRequests[from][to];
     }
+
     getGameRequests(id) {
-        if(!this.gameRequests.hasOwnProperty(id)) return [];
+        if (!this.gameRequests.hasOwnProperty(id)) return [];
         let requests = [];
-        for(let key of Object.keys(this.gameRequests[id]))
+        for (let key of Object.keys(this.gameRequests[id]))
             requests.push(this.gameRequests[id][key]);
         return requests;
     }
+
     acknowledgeRequestStatus(from, to) {
-        if(!this.gameRequests.hasOwnProperty(from) || !this.gameRequests[from].hasOwnProperty(to) || !this.gameRequests.hasOwnProperty(to) || !this.gameRequests[to].hasOwnProperty(from)) return;
+        if (!this.gameRequests.hasOwnProperty(from) || !this.gameRequests[from].hasOwnProperty(to) || !this.gameRequests.hasOwnProperty(to) || !this.gameRequests[to].hasOwnProperty(from)) return;
 
         let status = this.gameRequests[from][to].status;
-        if(status == 0) { //denied, delete request, do nothing
+        if (status == 0) { //denied, delete request, do nothing
             delete this.gameRequests[from][to];
             delete this.gameRequests[to][from];
+            this.createBackup()
         } else if (status == 1) { // accepted, delete request, start game
-            this.createGame(this.gameRequests[from][to].initiator, this.gameRequests[from][to].other);
+            this.createGame(this.gameRequests[from][to].initiator, this.gameRequests[from][to].other, this.gameRequests[from][to].initiator);
+
             delete this.gameRequests[from][to];
             delete this.gameRequests[to][from];
         }
-        this.createBackup()
     }
+
     confirmGameRequest(from, to, status) {
         // status: 0=denied, 1=accepted
-        if(!this.gameRequests.hasOwnProperty(from) || !this.gameRequests[from].hasOwnProperty(to) || !this.gameRequests.hasOwnProperty(to) || !this.gameRequests[to].hasOwnProperty(from)) return;
+        if (!this.gameRequests.hasOwnProperty(from) || !this.gameRequests[from].hasOwnProperty(to) || !this.gameRequests.hasOwnProperty(to) || !this.gameRequests[to].hasOwnProperty(from)) return;
         this.gameRequests[from][to].status = status;
         this.gameRequests[to][from].status = status;
         this.createBackup()
     }
-    createGame(initiator, other) {
-        if(!this.gameRequests.hasOwnProperty(initiator)) this.gameRequests[initiator] = {};
-        if(!this.gameRequests.hasOwnProperty(other)) this.gameRequests[other] = {};
+
+    createGame(initiator, other, start) {
+        if (!this.games.hasOwnProperty(initiator)) this.games[initiator] = {};
+        if (!this.games.hasOwnProperty(other)) this.games[other] = {};
+        let startingPlayer = start || initiator;
+
         let game = {
             initiator: initiator,
             other: other,
-            currentPlayer: initiator,
-            status: 0 // 0=waiting for prompts, 1=waiting for answer, 2=waiting for next game
+            currentPlayer: startingPlayer,
+            status: 0,
+            prompts: [],
+            answer: -1,
+            next: [0, 0]
         }
-        this.games[initiator] = game;
-        this.games[other] = game;
+        this.games[initiator][other] = game;
+        this.games[other][initiator] = game;
         this.createBackup()
+    }
+
+    getGame(from, to) {
+        if (!this.games.hasOwnProperty(from) || !this.games[from].hasOwnProperty(to)) return "null";
+        return this.games[from][to];
+    }
+
+    sendGamePrompts(from, to, prompts) {
+        if (!this.games.hasOwnProperty(from) || !this.games[from].hasOwnProperty(to)) return;
+        this.games[from][to].prompts = prompts;
+        this.games[to][from].prompts = prompts;
+
+        this.games[from][to].status = 1;
+        this.games[to][from].status = 1;
+        this.createBackup();
+    }
+
+    sendGameAnswer(from, to, answer) {
+        if (!this.games.hasOwnProperty(from) || !this.games[from].hasOwnProperty(to)) return;
+        this.games[from][to].answer = answer;
+        this.games[to][from].answer = answer;
+        this.games[from][to].status = 2;
+        this.games[to][from].status = 2;
+        this.createBackup();
+    }
+
+    newGame(from, to) {
+        if (!this.games.hasOwnProperty(from) || !this.games[from].hasOwnProperty(to)) return;
+        if (this.games[from][to].initiator == from) {
+            this.games[from][to].next[0] = 1;
+            this.games[to][from].next[0] = 1;
+        } else {
+            this.games[from][to].next[1] = 1;
+            this.games[to][from].next[1] = 1;
+        }
+
+        if (this.games[from][to].next[0] == 1 && this.games[from][to].next[1] == 1) {
+            let startingPlayer = this.games[from][to].currentPlayer == from ? to : from;
+            this.createGame(this.games[from][to].initiator, this.games[from][to].other, startingPlayer);
+        }
+
+        this.createBackup();
+    }
+    cancelGame(from, to) {
+        if (!this.games.hasOwnProperty(from) || !this.games[from].hasOwnProperty(to)) return;
+        if (this.games[from][to].initiator == from) {
+            this.games[from][to].next[0] = -1;
+            this.games[to][from].next[0] = -1;
+        } else {
+            this.games[from][to].next[1] = -1;
+            this.games[to][from].next[1] = -1;
+        }
+        this.createBackup();
+    }
+    finishGame(from, to) {
+        if (this.games.hasOwnProperty(from) && this.games[from].hasOwnProperty(to)) {
+            delete this.games[from][to];
+            delete this.games[to][from];
+        }
+
+        if (this.games.hasOwnProperty(to) && this.games[to].hasOwnProperty(from)) {
+            delete this.games[from][to];
+            delete this.games[to][from];
+        }
     }
 }
 
--- server/routes/minigame.js
+++ server/routes/minigame.js
@@ -11,6 +11,11 @@ router.get('/request', (req, res) => {
     let to = req.query.to;
     res.send(serverState.getGameRequest(from, to)).status(200).end();
 });
+router.get('/game', (req, res) => {
+    let from = req.query.from;
+    let to = req.query.to;
+    res.send(serverState.getGame(from, to)).status(200).end();
+})
 router.post('/acknowledge-request-status', (req, res) => {
     let from = req.body.from;
     let to = req.body.to;
@@ -28,6 +33,38 @@ router.post('/request-game', (req, res) => {
     let from = req.body.from;
     let to = req.body.to;
     serverState.requestGame(from, to);
+    res.status(200).end();
+});
+router.post('/add-prompts', (req, res) => {
+    let from = req.body.from;
+    let to = req.body.to;
+    let prompts = JSON.parse(req.body.prompts);
+    serverState.sendGamePrompts(from, to, prompts);
+    res.status(200).end();
+});
+router.post('/add-answer', (req, res) => {
+    let from = req.body.from;
+    let to = req.body.to;
+    let answer = req.body.answer;
+    serverState.sendGameAnswer(from, to, answer);
+    res.status(200).end();
+});
+router.post('/new-game', (req, res) => {
+    let from = req.body.from;
+    let to = req.body.to;
+    serverState.newGame(from, to);
+    res.status(200).end();
+});
+router.post('/cancel-game', (req, res) => {
+    let from = req.body.from;
+    let to = req.body.to;
+    serverState.cancelGame(from, to);
+    res.status(200).end();
+});
+router.post('/finish-game', (req, res) => {
+    let from = req.body.from;
+    let to = req.body.to;
+    serverState.finishGame(from, to);
     res.status(200).end();
 });
 module.exports = {url: "/game", router: router};
