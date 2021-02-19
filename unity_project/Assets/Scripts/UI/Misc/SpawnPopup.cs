b842764e8ebacc4991ed7cfbf8268bb0bc49ee98
Vlad Ilie Miu <miu.vlad00@gmail.com>
2021-02-19 15:44:12 +0200

    Removed scripts, added popup tester

---
+++ unity_project/Assets/Scripts/UI/Misc/SpawnPopup.cs
@@ -0,0 +1,23 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class SpawnPopup : MonoBehaviour
+{
+    [SerializeField]private GameObject popupPrefab;
+    [SerializeField]private string popupText;
+    private GameObject _clickedBtn;
+
+    // private void Initialize() {
+    //     
+    // }
+
+    public void OnClick()
+    {
+        _clickedBtn = this.gameObject;
+        var popupObj = Instantiate(popupPrefab, transform.parent);
+        var textComp = popupObj.GetComponent<TMP_Text>();
+        textComp.text = _clickedBtn + popupText;
+        
+    }
+    
+}
